ceb6b76af1c9b9498ba39e4d1e8a1a8fd5098db8
Tavaris Thompson <varis93@yahoo.com>
2021-11-12 12:57:21 -0500

    thrid trial

--- assets/js/script.js
+++ assets/js/script.js
@@ -200,6 +200,46 @@ function checkForEnter(event) {
       saveHighscore();
 }
 
+function printHighscores() {
+    // either get scores from localstorage or set to empty array
+    var highScores = JSON.parse(localStorage.getItem("highscores"));
+    if(highScores != null){
+      // (optional) sort highscores by score property in descending order
+      highScores.sort(function(a, b) {
+        return parseInt(b.score) - parseInt(a.score);
+      });
+      // for each score
+      for(var i = 0; i < highScores.length; i++){
+        // create li tag for each high score
+        var scoreLi = document.createElement("li");
+        scoreLi.textContent = highScores[i].initials + " - " + highScores[i].score;
+        // display on page
+        document.getElementById("highscores").appendChild(scoreLi);
+      }
+      
+    }
+    else{
+      var temp = document.getElementById("highscores");
+      temp.textContent = "NO HIGH SCORES";     
+    }
+    
+      
+  }
+  
+  function clearHighscores() {
+    // (and reload)
+    localStorage.removeItem("highscores");
+    location.reload();
+  }
+  
+  // attach clear event to clear score button
+  var clearButton = document.getElementById("clear");
+  clearButton.addEventListener("click", function(){
+    clearHighscores();
+  })
+  // run printhighscore when page loads
+  printHighscores();
+
 // user clicks button to submit initials
 submitBtn.onclick = saveHighscore;
 
