b030f3a59a948215a8d0b503b76c08434bb8206c
Tavaris Thompson <varis93@yahoo.com>
2021-11-14 14:14:43 -0500

    error

--- assets/js/script.js
+++ assets/js/script.js
@@ -200,34 +200,6 @@ function checkForEnter(event) {
       saveHighscore();
 }
 
-function printHighscores() {
-    // either get scores from localstorage or set to empty array
-    var highScores = JSON.parse(localStorage.getItem("highscores"));
-    if(highScores != null){
-      // (optional) sort highscores by score property in descending order
-      highScores.sort(function(a, b) {
-        return parseInt(b.score) - parseInt(a.score);
-      });
-      // for each score
-      for(var i = 0; i < highScores.length; i++){
-        // create li tag for each high score
-        var scoreLi = document.createElement("li");
-        scoreLi.textContent = highScores[i].initials + " - " + highScores[i].score;
-        // display on page
-        document.getElementById("highscores").appendChild(scoreLi);
-      }
-      
-    }
-    else{
-      var temp = document.getElementById("highscores");
-      temp.textContent = "NO HIGH SCORES";     
-    }
-    
-      
-  }
-  
-
-
 // user clicks button to submit initials
 submitBtn.onclick = saveHighscore;
 
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -9,5 +9,26 @@ function clearHighscores() {
   clearButton.addEventListener("click", function(){
     clearHighscores();
   })
-  // run printhighscore when page loads
-  printHighscores();
+
+  function printHighscores() {
+    // either get scores from localstorage or set to empty array
+    var highScores = JSON.parse(localStorage.getItem("highscores"));
+    if(highScores != null){
+      // (optional) sort highscores by score property in descending order
+      highScores.sort(function(a, b) {
+        return parseInt(b.score) - parseInt(a.score);
+      });
+      // for each score
+      for(var i = 0; i < highScores.length; i++){
+        // create li tag for each high score
+        var scoreLi = document.createElement("li");
+        scoreLi.textContent = highScores[i].initials + " - " + highScores[i].score;
+        // display on page
+        document.getElementById("highscores").appendChild(scoreLi);
+      }
+      
+    }
+    else{
+      var temp = document.getElementById("highscores");
+      temp.textContent = "NO HIGH SCORES";
+    }
